cddd5bd067b60a32e91507a04eac4c355c87344e
collins <collinsleonkimani@outlook.com>
2023-03-09 02:50:43 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    const thankYouMessages = [];
+  
+    for (let i = 0; i < namesArray.length; i++) {
+      const thankYouMessage = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
+      thankYouMessages.push(thankYouMessage);
+    }
+  
+    return thankYouMessages;
+  }
+  
+
